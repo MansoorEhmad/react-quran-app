9ecb0d8af415d0fd27453751a4810182961ea82b
Khalid Mansoor <97522481+MansoorEhmad@users.noreply.github.com>
2024-06-10 06:04:37 +0500

    changes
    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
                         <img src="/images/2.jpeg" className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="..." className="d-block w-100" alt="..." />
+                        <img src="/images/3.jpeg" className="d-block w-100" alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
